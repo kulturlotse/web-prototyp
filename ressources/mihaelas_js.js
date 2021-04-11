7b9e4c77bc9d79a493e08c8591b8bc9ec2b15846
Mihaela-Catalina Manolescu <mihaela-catalina.manolescu@otto.de>
2021-04-11 13:07:45 +0200

    prototyp homepage (see index_Mihaela.html)

---
+++ ressources/mihaelas_js.js
@@ -0,0 +1,6 @@
+(function () {
+  'use strict'
+  document.querySelector('[data-bs-toggle="offcanvas"]').addEventListener('click', function () {
+    document.querySelector('.offcanvas-collapse').classList.toggle('open')
+  })
+})()
